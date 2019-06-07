10715bcd02ed87531c559e9b5a7624e08ec28d2f
Hemang <hemang249@gmail.com>
2019-06-08 00:29:20 +0530

    Modified gitignore

--- Assets/Scripts/TorchScript.cs
+++ Assets/Scripts/TorchScript.cs
@@ -5,11 +5,40 @@ using UnityEngine;
 public class TorchScript : MonoBehaviour
 {
    public Light Torch;
+   [SerializeField] int numberOfTorches = 4;    // Number of torches player will spawn with
+   public float torchLife = 5f;    // Time the torch will last 
+   private bool isUsingTorch = false;
 
    
    void Start()
    {
-       TorchFlicker();
+       Torch.intensity = 0f;
+   }
+
+   void Update()
+   {
+       if( Input.GetKey(KeyCode.Space) && !isUsingTorch)
+       {
+            UseTorch();
+            
+       }    
+   }
+
+   void UseTorch()
+   {
+       
+        Torch.intensity = 1f;
+        isUsingTorch = true;
+        Invoke("TorchTimer",torchLife);  // Destroy torch after specified Time
+        TorchFlicker();
+        numberOfTorches --;
+   }
+
+   void TorchTimer()
+   {
+       Torch.intensity = 0;
+       isUsingTorch = false;
+       // Destroy(Torch); Uncomment after adding Torch sprite
    }
 
    void TorchFlicker()
@@ -18,6 +47,10 @@ public class TorchScript : MonoBehaviour
        float intensity = Random.Range(0.5f , 2.5f);
        Torch.range = flickerRange;
        Torch.intensity = intensity;
-       Invoke("TorchFlicker", 0.5f);
+
+       if(isUsingTorch)
+           Invoke("TorchFlicker", 0.5f);
+       else
+           Torch.intensity = 0f;
    }
 }
