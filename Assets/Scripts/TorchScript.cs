b0d18a5b7881a13d4a14c22df4c254fbaa748b30
Hemang <hemang249@gmail.com>
2019-06-07 20:55:52 +0530

    Torch flicker Added

---
+++ Assets/Scripts/TorchScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchScript : MonoBehaviour
+{
+   public Light Torch;
+
+   
+   void Start()
+   {
+       TorchFlicker();
+   }
+
+   void TorchFlicker()
+   {
+       float flickerRange = Random.Range(15f , 20f);
+       float intensity = Random.Range(0.5f , 2.5f);
+       Torch.range = flickerRange;
+       Torch.intensity = intensity;
+       Invoke("TorchFlicker", 0.5f);
+   }
+}
