d1ea89ca2b481cf34f1c9920738fb9ee860929fa
Hemang <hemang249@gmail.com>
2019-06-07 18:18:41 +0530

    Added Animation And Simplified Code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,18 +11,18 @@ public class PlayerController : MonoBehaviour
     [Header("Character Attributes")]
     [Range(0,15)]
     public float movementBaseSpeed = 5.0f;
+
     [Space]
     [Header("Character Statistics")]
-    public Vector2 movementDirection;
+    public Vector3 movementDirection;
     public float movementSpeed;
-    [Space]
-    [Header("References")]
-    public Rigidbody2D rb;
+
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -35,14 +35,26 @@ public class PlayerController : MonoBehaviour
 
     void ProcessInputs()
     {
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        movementDirection = new Vector3(xAxis, yAxis  , 0);
         movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
         movementDirection.Normalize();
+
     }
 
     void Move()
     {
-        rb.velocity = movementDirection * movementSpeed * movementBaseSpeed;
+       transform.position += movementBaseSpeed * movementDirection * Time.deltaTime;
+
+       if(movementDirection.y != 0)     // If moving along the  Y Axis , Play the Top Down Animation
+       {
+           animator.SetBool("isWalkingTopDown",true);
+       }
+       else
+       {
+           animator.SetBool("isWalkingTopDown",false);
+       }
     }
 
 }
