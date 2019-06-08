1beb5dd84260327631494abab72212ff28b5fff1
MarkSoundDev <marksounddev@gmail.com>
2019-06-08 00:02:31 -0400

    Modified Follow Camera to use Cinemachine.  Better Camera Dynamics.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[DefaultExecutionOrder(-100)]
 public class PlayerController : MonoBehaviour
 {
     [Header("Input Settings")]
