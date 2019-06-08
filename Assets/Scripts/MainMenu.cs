1130c5334fa531d6e8d307752cff88b75d3f5d45
MarkSoundDev <marksounddev@gmail.com>
2019-06-08 03:15:11 -0400

    Project Cleanup.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,12 +8,15 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
-    public void PlayGameWithWait()
+    /**
+     public void PlayGameWithWait()
     {
         Invoke ("PlayGame", 1.0f);
     }
+    **/
 
     public AudioSource uiSounds;
     public AudioClip hoverSound;
