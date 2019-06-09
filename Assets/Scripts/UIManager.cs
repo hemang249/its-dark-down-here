01e401d38d1990c348f2a76b4b828896d713e233
MarkSoundDev <marksounddev@gmail.com>
2019-06-09 03:27:45 -0400

    Minimap Built.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIManager : MonoBehaviour
@@ -11,6 +12,26 @@ public class UIManager : MonoBehaviour
      
     // Other scripts access this one through its public static methods.
     public TextMeshProUGUI fearTime;        //Text element showing time in dark
+
+    [SerializeField]
+    private float fearAmount;
+
+    private 
+
+    void update()
+    {
+        //fearTime.text = PlayerController.globalDarkTime();
+
+        FearBar();
+
+
+    }
+
+    private void FearBar()
+    {
+        
+    }
+
 
     /**
     public static void UpdateFearUI(float time)
@@ -23,5 +44,5 @@ public class UIManager : MonoBehaviour
         fearTime.text = PlayerController.globalDarkTime(time);
     }
     **/
-    
+
 }
