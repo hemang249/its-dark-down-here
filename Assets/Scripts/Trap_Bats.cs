a4db719df852c84876b24e3443fed9b6dfb1afdd
MarkSoundDev <marksounddev@gmail.com>
2019-06-11 05:48:59 -0400

    Bat traps made.

---
+++ Assets/Scripts/Trap_Bats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_Bats : MonoBehaviour
+{
+
+    public AudioSource batSound;
+    public ParticleSystem batParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        batSound.Play();
+        batParticles.Play();
+        
+    }
+}
