1a6f30dacee850ab2e0cec6d875322c9adff9057
Hemang <hemang249@gmail.com>
2019-06-13 16:37:20 +0530

    Loading Working

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -18,7 +18,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         ClickSound();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Loading");
 
     }
 
